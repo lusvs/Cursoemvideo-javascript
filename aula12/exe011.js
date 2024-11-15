6979a32c36082ec0abeec851a61d8a6aab00a262
Lucas <lucasvssoares@gmail.com>
2024-11-15 20:27:50 -0300

    feat: aula 12, ex011

---
+++ aula12/exe011.js
@@ -0,0 +1,13 @@
+var idade = 15
+
+if (idade < 16) {
+    console.log(`Sua idade é ${idade} anos, você Não vota`)
+}
+else if(idade > 66 || idade < 18){   
+    console.log(`Sua idade é ${idade} anos, você PODE votar`)
+}
+else{
+    console.log(`Sua idade é ${idade} anos,  você TEM que votar`)
+}
+
+
