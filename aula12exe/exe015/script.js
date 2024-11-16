5e9a95afe237956b5ddad03161260f8645120da5
Lucas <lucasvssoares@gmail.com>
2024-11-16 19:26:04 -0300

    feat: aula 12, ex015

---
+++ aula12exe/exe015/script.js
@@ -0,0 +1,65 @@
+function verificar(){
+    var data = new Date()
+
+    // Fala que a var 'ano' representa o ano atual
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+
+    // Se é igual a zero ou maior que o ano real
+    if (fano.value.length == 0 || fano.value > ano){
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    // Se estiver tudo correto
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        // Se for homem
+        if(fsex[0].checked){
+            genero = 'Homem'
+            if (idade >=0 && idade < 13){
+                //Criança
+                img.setAttribute('src', 'img/menino.jpg')
+            }else if (idade < 18){
+                //Jovem
+                img.setAttribute('src', 'img/garoto.jpg')
+            }else if (idade < 60){
+                //Adulto
+                img.setAttribute('src', 'img/adulto.jpg')
+            } else{
+                //Idoso
+                img.setAttribute('src', 'img/idoso.jpg')
+            }
+        // Se for mulher
+        } else if (fsex[1].checked){
+            genero = 'Mulher'
+            if (idade >=0 && idade < 13){
+                //Criança
+                img.setAttribute('src', 'img/menina.jpg')
+            }else if (idade < 18){
+                //Jovem
+                img.setAttribute('src', 'img/garota.jpg')
+            }else if (idade < 60){
+                //Adulto
+                img.setAttribute('src', 'img/adulta.jpg')
+            } else{
+                //Idoso
+                img.setAttribute('src', 'img/idosa.jpg')
+            }
+        }
+        // Faz a resposta ir pro meio
+        res.style.textAlign = 'center'
+
+        // Faz trocar a frase dps de vericar
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+
+        // Faz a img aparecer dps de verificar
+        res.appendChild(img)
+
+        // Arruma o tamanho da img
+        img.style.height = '200px'
+    }
+}
