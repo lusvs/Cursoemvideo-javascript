4c84b90af086c25ac9940a86af00568293ab98a0
Lucas <lucasvssoares@gmail.com>
2024-11-17 13:33:30 -0300

    feat: aula 13, loopings

--- aula13/loopings.js
+++ aula13/loopings.js
@@ -1,14 +1,15 @@
 var c = 1
+var a = 1
 
 // Faz o teste lógico antes, se for true ele executa.
-// while (c <= 6){
-//     console.log(`Passo ${c}`)
-//     c++
-// }
-
+while (c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+}
+console.log('=======================================')
 // Executa o comando antes, se depois for true ele testa novamente.
-// do {
-//     console.log(`Passo ${c}`)
-//     c++
-// } while (c <= 10)
+do {
+    console.log(`Passo ${a}`)
+    a++
+} while (a <= 10)
 
